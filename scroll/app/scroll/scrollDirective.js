d0770017e3f9cccae28348534cbaa05da578d4d5
JS4lcaEq <condorrr@gmail.com>
2016-12-09 14:54:26 +0300

    dasdasda

--- scroll/app/scroll/scrollDirective.js
+++ scroll/app/scroll/scrollDirective.js
@@ -46,31 +46,31 @@
             var elements = {
                 box: element.find(".va-scroll")
                 , spacer: element.find(".va-scroll-spacer")
-                , window: element.find(".va-scroll-window") 
+                , window: element.find(".va-scroll-window")
             };
 
             var current = {
-                heights: { item: 19, spacer: 0, box: 0 }
+                heights: { item: 20, spacer: 0, box: 0 }
                 , indexes: { start: 0, end: 0, max: 0 }
                 , windowLength: 10
                 , triggers: {}
                 , counts: { scroll: 0 }
                 , timers: { scroll: date.getTime() }
-                , intervals: {scroll: null}
+                , intervals: { scroll: null }
                 , stat: []
                 , bussy: false
                 , scroll: 0
             };
 
-            scope.counts = {scroll: 0};
+            scope.counts = { scroll: 0 };
 
             scope.$watch("vaSrc", function (value) {
                 if (value) {
                     current.indexes.max = value.length - 1;
                     current.heights.box = elements.box.height();
                     current.heights.spacer = getSpacerHeight(scope.vaSrc.length, current.windowLength, current.heights.item, current.heights.box);
-                    elements.spacer.height(current.heights.spacer);
-                    elements.window.height(current.heights.box);
+                    //elements.spacer.height(current.heights.spacer);
+                    //elements.window.height(current.heights.box);
                     setIndexes(current.indexes.start);
                 }
             });
@@ -82,33 +82,43 @@
             });
 
             elements.box.on("scroll", function (e) {
- 
                 var scroll = elements.box[0].scrollTop;
-                var dScroll = Math.abs(scroll - current.scroll);
-                
-                if (dScroll > 18 || scroll < 10 || scroll > (current.heights.spacer - 10) ) {
-                    var ntrvl = 1;
-                    if (current.bussy) {
-                        ntrvl = 50;
+                if (scroll != 50) {
+                    if (scroll > 50) {
+                        setIndexes(current.indexes.start + 1);
+                    } else {
+                        setIndexes(current.indexes.start - 1);
                     }
-                    current.scroll = scroll;
-                    elements.window.css("margin-top", scroll + "px");
-                    console.log(scroll);
-                    if (current.intervals.scroll) {
-                        $timeout.cancel(current.intervals.scroll);
-                    }
-                    current.intervals.scroll = $timeout(function () {
-                        current.bussy = true;
-                        var si = Math.round(scroll / current.heights.item);
-                    
-                        $timeout(function () {
-                            setIndexes(si);
-                            current.bussy = false;
-                        }, 1);               
-                    }, ntrvl);
+                    elements.box.scrollTop(50);
+                    console.log("scroll", scroll, "current.indexes.start", current.indexes.start);
+                    scope.$apply();
                 }
 
-                
+                //var dScroll = Math.abs(scroll - current.scroll);
+
+                //if (dScroll >= current.heights.item || scroll < current.heights.item || scroll > (current.heights.spacer - current.heights.item)) {
+                //    var ntrvl = 1;
+                //    if (current.bussy) {
+                //        ntrvl = 50;
+                //    }
+                //    current.scroll = scroll;
+                //    elements.window.css("margin-top", scroll + "px");
+                //    console.log(scroll);
+                //    if (current.intervals.scroll) {
+                //        $timeout.cancel(current.intervals.scroll);
+                //    }
+                //    current.intervals.scroll = $timeout(function () {
+                //        current.bussy = true;
+                //        var si = Math.round(scroll / current.heights.item);
+
+                //        $timeout(function () {
+                //            setIndexes(si);
+                //            current.bussy = false;
+                //        }, 1);               
+                //    }, ntrvl);
+                //}
+
+
             });
 
             elements.box.on("resize", function (e) {
@@ -118,8 +128,10 @@
             });
 
             function setIndexes(startIndex) {
+                if (startIndex && startIndex >= 0 && startIndex <= current.indexes.max) {
+                    current.indexes.start = startIndex;
 
-                current.indexes.start = startIndex;
+                }
                 current.indexes.end = getEndIndex(current.indexes.start, current.windowLength, current.indexes.max);
                 setWindow(scope.vaWindow, scope.vaSrc, current.indexes.start, current.indexes.end);
 
